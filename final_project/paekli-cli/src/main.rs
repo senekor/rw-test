636b8bff8bb9f70b938d7ed6c6b188028f89687f
Remo Senekowitsch <remo@buenzli.dev>
2024-03-06 12:43:42 +0100

    implement sending of paekli

--- final_project/paekli-cli/src/main.rs
+++ final_project/paekli-cli/src/main.rs
@@ -1,13 +1,13 @@
 use clap::{Parser, Subcommand};
 
-#[derive(Debug, Subcommand)]
+#[derive(Subcommand)]
 enum Command {
-    Send,
+    Send { content: String },
     Receive,
 }
 
 /// send and receive joy with ✨ paekli-cli ✨
-#[derive(Debug, Parser)]
+#[derive(Parser)]
 #[clap(version)]
 struct Cli {
     #[command(subcommand)]
@@ -16,18 +16,25 @@ struct Cli {
 
 const SEND_MESSAGE: &str = "\
 Thank you for trusting Paekli AG!
-We will deliver your paekli in mint condition.
-* throws your paekli directly in the trash *";
+We will deliver your paekli in mint condition.";
 
 const RECEIVE_MESSAGE: &str = "\
 There aren't any paekli for you at the moment.
-* tries to hide paekli in the trash can *";
+* tries to hide paekli on the storage shelf *";
 
 fn main() {
     let args = Cli::parse();
 
+    let project_dir = directories::ProjectDirs::from("dev", "buenzli", "paekli")
+        .expect("the user's home directory seems to be corrupt");
+    let storage_dir = project_dir.data_dir();
+    std::fs::create_dir_all(storage_dir).expect("failed to create storage directory");
+
     match args.command {
-        Command::Send => println!("{SEND_MESSAGE}"),
+        Command::Send { content } => {
+            std::fs::write(storage_dir.join("content"), content).expect("failed to store paekli");
+            println!("{SEND_MESSAGE}");
+        }
         Command::Receive => println!("{RECEIVE_MESSAGE}"),
     }
 }
