2fa1c230cf9a3ac6ac5be080c582e3b716d5931f
Remo Senekowitsch <remo@buenzli.dev>
2024-03-06 19:58:44 +0100

    implement individual receivers

--- final_project/paekli-cli/src/main.rs
+++ final_project/paekli-cli/src/main.rs
@@ -3,8 +3,15 @@ use clap::{Parser, Subcommand};
 
 #[derive(Subcommand)]
 enum Command {
-    Send { content: String },
-    Receive,
+    Send {
+        content: String,
+        #[arg(long("to"))]
+        receiver: String,
+    },
+    Receive {
+        #[arg(long("for"))]
+        receiver: String,
+    },
 }
 
 /// send and receive joy with ✨ paekli-cli ✨
@@ -20,19 +27,20 @@ Thank you for trusting Paekli LLC!
 We will deliver your paekli in mint condition.";
 
 fn main() -> anyhow::Result<()> {
-    println!("Hello Silvia!");
-
     let args = Cli::parse();
 
     let project_dir = directories::ProjectDirs::from("dev", "buenzli", "paekli")
         .context("the user's home directory seems to be corrupt")?;
     let storage_dir = project_dir.data_dir();
-    std::fs::create_dir_all(storage_dir).context("failed to create storage directory")?;
 
     match args.command {
-        Command::Send { content } => {
+        Command::Send { content, receiver } => {
+            let receiver_dir = storage_dir.join(receiver);
+            std::fs::create_dir_all(&receiver_dir)
+                .context("failed to create receiver directory")?;
+
             let time = time::OffsetDateTime::now_utc().to_string();
-            let paekli_path = storage_dir.join(time);
+            let paekli_path = receiver_dir.join(time);
 
             if std::fs::metadata(&paekli_path).is_ok() {
                 anyhow::bail!("Cannot send paekli, storage is full.");
@@ -40,9 +48,12 @@ fn main() -> anyhow::Result<()> {
             std::fs::write(paekli_path, content).context("failed to store paekli")?;
             println!("{SEND_MESSAGE}");
         }
-        Command::Receive => {
-            let mut paekli: Vec<_> = std::fs::read_dir(storage_dir)
-                .context("failed to read from storage")?
+        Command::Receive { receiver } => {
+            let receiver_dir = storage_dir.join(&receiver);
+
+            let mut paekli: Vec<_> = std::fs::read_dir(&receiver_dir)
+                .into_iter()
+                .flatten()
                 .flatten()
                 .map(|e| e.file_name())
                 .collect();
@@ -50,12 +61,12 @@ fn main() -> anyhow::Result<()> {
             let paekli_name = paekli
                 .into_iter()
                 .next()
-                .context("There is no paekli to receive.")?
+                .context(format!("There is no paekli for {}.", receiver))?
                 .into_string()
                 .ok()
                 .context("paekli name should be a utf-8 string")?;
 
-            let paekli_path = storage_dir.join(paekli_name);
+            let paekli_path = receiver_dir.join(paekli_name);
 
             match std::fs::read_to_string(&paekli_path) {
                 Ok(content) => {
