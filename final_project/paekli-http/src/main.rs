15187140f8e108599be0b5be09d4a67280f57df8
Remo Senekowitsch <remo@buenzli.dev>
2024-03-09 15:17:40 +0100

    stub out send and receive http handlers

--- final_project/paekli-http/src/main.rs
+++ final_project/paekli-http/src/main.rs
@@ -1,12 +1,25 @@
-use axum::routing::get;
+use axum::routing::{delete, post};
 
-async fn hello_world() -> &'static str {
-    "hello world"
+#[axum::debug_handler]
+async fn send_paekli() -> &'static str {
+    "\
+Thank you for trusting Paekli LLC!
+We will deliver your paekli in mint condition.
+* throws your paekli directly in the trash *"
+}
+
+#[axum::debug_handler]
+async fn receive_paekli() -> &'static str {
+    "\
+There aren't any paekli for you at the moment.
+* tries to hide paekli in the trash can *"
 }
 
 #[tokio::main]
 async fn main() {
-    let router = axum::Router::new().route("/", get(hello_world));
+    let router = axum::Router::new()
+        .route("/", post(send_paekli))
+        .route("/", delete(receive_paekli));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
