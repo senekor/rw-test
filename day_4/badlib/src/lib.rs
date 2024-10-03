74ab9e60323878f2edf63058cccfb2ab9d87a3ae
Remo Senekowitsch <remo@buenzli.dev>
2024-10-03 20:00:22 +0200

    fix additional lints

--- day_4/badlib/src/lib.rs
+++ day_4/badlib/src/lib.rs
@@ -5,27 +5,31 @@
 //! We will incrementally build a CI pipeline to discover
 //! how CI can help us make sure our Rust code is squeaky clean.
 
+/// fn
 pub fn badly_formatted() {
     println!("This code is badly formatted.");
     println!("Don't change it until CI fails because of it.");
 }
 
+/// fn
 pub fn clippy_doesnt_like_this() {
     if i32::MAX > i32::MIN {
         println!("This is ridiculous.");
     }
 }
 
-pub fn dangerous_cast(n: i64) -> i32 {
+/// fn
+pub fn dangerous_cast(n: i64) -> Result<i32, std::num::TryFromIntError> {
     // In my opinion, the `as` keyword should be used sparingly.
     // This conversion is lossy and may not do what you expect.
     // It is better to replace it with `.try_into()` and return
     // a `Result<i64, _>` instead.
     //
     // But as always, let's get CI to complain about it before we fix it.
-    n as i32
+    n.try_into()
 }
 
+/// add
 pub fn add(left: usize, right: usize) -> usize {
     // I don't know how to add two numbers...
     left - right
